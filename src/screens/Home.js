2d8288e2352d7417899f2c35ea57dc7de3db88a7
Ajit kumar <kajit0408@gmail.com>
2023-07-02 17:57:25 +0530

    folders changed

---
+++ src/screens/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      body
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
